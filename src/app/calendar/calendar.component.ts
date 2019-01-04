cacf465f43351684d296614a25d17d3b54847672
Stanislav Zhurskyi <szhurstc@training.local>
2019-01-04 22:53:37 +0200

    Fix main-page Add padding Remove json info

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -55,27 +55,6 @@ import {
   
     viewDate: Date = new Date();
   
-    modalData: {
-      action: string;
-      event: CalendarEvent;
-    };
-  
-    actions: CalendarEventAction[] = [
-      {
-        label: '<i class="fa fa-fw fa-pencil"></i>',
-        onClick: ({ event }: { event: CalendarEvent }): void => {
-          this.handleEvent('Edited', event);
-        }
-      },
-      {
-        label: '<i class="fa fa-fw fa-times"></i>',
-        onClick: ({ event }: { event: CalendarEvent }): void => {
-          this.events = this.events.filter(iEvent => iEvent !== event);
-          this.handleEvent('Deleted', event);
-        }
-      }
-    ];
-  
     refresh: Subject<any> = new Subject();
   
     events: CalendarEvent[] = [];
@@ -107,13 +86,7 @@ import {
     }: CalendarEventTimesChangedEvent): void {
       event.start = newStart;
       event.end = newEnd;
-      this.handleEvent('Dropped or resized', event);
       this.refresh.next();
-    }
-  
-    handleEvent(action: string, event: CalendarEvent): void {
-      this.modalData = { event, action };
-      this.modal.open(this.modalContent, { size: 'lg' });
     }
   
     addEvent(): void {
