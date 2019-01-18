7cedcfe642699e88de6637638a110542f602f03a
nnikita8 <nikita.nosadchuk@gmail.com>
2019-01-19 00:57:40 +0300

    Fixed adding groups by mentor.

--- src/app/groups/add-group/add-group.component.ts
+++ src/app/groups/add-group/add-group.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { GroupService } from '../../common/services/group.service';
 import { AlertWindowsComponent } from '../../components/alert-windows/alert-windows.component';
 import { HttpStatusCodeService } from '../../common/services/http-status-code.service';
 import { AuthService } from '../../common/services/auth.service';
 import { Group } from '../../common/models/group';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { HttpResponse, HttpErrorResponse } from '../../../../node_modules/@angular/common/http';
 
 @Component({
@@ -19,8 +19,13 @@ export class AddGroupComponent implements OnInit {
     private httpStatusCodeService: HttpStatusCodeService,
     public thisDialogRef: MatDialogRef<AddGroupComponent>,
     private alertwindow: AlertWindowsComponent,
-    private groupService: GroupService) { }
+    private groupService: GroupService,
+    @Inject(MAT_DIALOG_DATA) public data) {
+      this.groups = data;
+     }
 
+  group: Group;
+  groups: Group[];
   progresSpinerActive: boolean;
   errorMessage: string;
   errorMessageActive = false;
@@ -41,17 +46,25 @@ export class AddGroupComponent implements OnInit {
   createGroup(form: NgForm) {
     this.progresSpinerActive = true;
     form.form.disable();
-    const group: Group = {
-      Id: 0,
+    const group = {
       Name: form.value.Name,
       MentorId: this.authService.getUserId(),
       MentorName: this.authService.getUserFullName()
     };
-    this.groupService.createGroup(group).subscribe(
+    this.groupService.createGroup(group as Group).subscribe(
       resp => {
         if (this.httpStatusCodeService.isOk(resp.status)) {
           this.someGroupCreated = true;
-          this.alertwindow.openSnackBar(resp.body, 'Ok');
+          this.alertwindow.openSnackBar("Succesfully created group: \"" + group.Name + "\".", 'Ok');
+
+          this.group = {
+            Id : resp.body,
+            Name : group.Name,
+            MentorId : group.MentorId,
+            MentorName : group.MentorName
+          }
+          this.groups.push(this.group);
+          this.thisDialogRef.close();
         }
       },
       error => {
